6fa424e7ff30ae1c27e021395bd768fc10679a0d
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-12-04 15:46:31 +0200

    Update crieateWatched.js

--- src/js/crieateWatched.js
+++ src/js/crieateWatched.js
@@ -3,19 +3,23 @@ const refs = {
   gallery: document.querySelector('.gallery'),
   loadMore: document.querySelector('.load-more'),
 };
+let watchedPhoto = [];
 
 export default function crieateWatchedGallery() {
   refs.gallery.innerHTML = '';
   const arrey = JSON.parse(localStorage.getItem('watched'));
-  console.log(arrey);
+  // console.log(arrey);
   try {
     for (const photo of arrey) {
       const value = JSON.parse(photo);
-      console.log(value);
+      // console.log(value);
+      watchedPhoto.push(value);
+      // console.log(watchedPhoto);
     }
   } catch (error) {
     console.log(error.name);
     console.log(error.message);
   }
+  crieateGallery(watchedPhoto);
   refs.loadMore.classList.add('visually-hidden');
 }
